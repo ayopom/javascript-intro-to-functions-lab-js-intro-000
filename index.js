28990ac9728e80545a257b67c88b2c29492a92be
Ayodele Pompey <ayodelepompey@gmail.com>
2019-02-27 14:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
   else if (string === str1.toUpperCase()) {
     return "YES INDEED!";
   }
-  else if (string === "I lov you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 
